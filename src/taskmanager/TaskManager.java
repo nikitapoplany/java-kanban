169c5089ef2f8349b327458060e82182fdfb7428
nikitanovikov <Vertihost@yandex.com>
2025-07-21 23:18:43 +0300

    Fix: Whitespaces

--- src/taskmanager/TaskManager.java
+++ src/taskmanager/TaskManager.java
@@ -13,26 +13,40 @@ public interface TaskManager {
     // Методы для работы с задачами
     List<Task> getAllTasks();
     Task getTaskById(int id);
+
     void createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTaskById(int id);
+
     void deleteAllTasks();
 
     // Методы для работы с эпиками
     List<Epic> getAllEpics();
+
     Epic getEpicById(int id);
+
     void createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
+
     void deleteAllEpics();
 
     // Методы для работы с подзадачами
     List<Subtask> getAllSubtasks();
     Subtask getSubtaskById(int id);
+
     void createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtaskById(int id);
+
     void deleteAllSubtasks();
+
     List<Subtask> getSubtasksByEpicId(int epicId);
 
     // Метод для получения истории просмотров
